d4e9e832921e807c6b568cc7cef00bcd9c2d57ab
Alper <akcanalpermutlu@gmail.com>
2026-04-28 01:42:38 +0300

    adds product-service test files

---
+++ backend/product-service/src/test/java/com/n11_alpermutluakcan/product_service/ProductServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n11_alpermutluakcan.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
