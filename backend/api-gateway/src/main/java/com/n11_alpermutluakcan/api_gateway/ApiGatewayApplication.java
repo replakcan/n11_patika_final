a773687606429aab3d4769e5dbadd19088038890
Alper <akcanalpermutlu@gmail.com>
2026-04-28 01:45:00 +0300

    adds api-gateway initial files
    makes api-gatewat eureka-client
    and adds simple configs

--- backend/api-gateway/src/main/java/com/n11_alpermutluakcan/api_gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/n11_alpermutluakcan/api_gateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.n11_alpermutluakcan.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
