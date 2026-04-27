f4ae73950c864de33bb43df137e811c1d5bf493f
Alper <akcanalpermutlu@gmail.com>
2026-04-28 01:38:07 +0300

    creates product-repositroy interface
    extends Jpa-repository

--- backend/product-service/src/main/java/com/n11_alpermutluakcan/product_service/repository/ProductRepository.java
+++ backend/product-service/src/main/java/com/n11_alpermutluakcan/product_service/repository/ProductRepository.java
@@ -1,6 +1,6 @@
-package com.n11_alpermutluakcan.product_server.repository;
+package com.n11_alpermutluakcan.product_service.repository;
 
-import com.n11_alpermutluakcan.product_server.entity.Product;
+import com.n11_alpermutluakcan.product_service.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
