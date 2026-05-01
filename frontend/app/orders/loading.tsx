c1077f6e2c38bfdc19d67370128172056cbba991
Alper <akcanalpermutlu@gmail.com>
2026-05-01 13:41:57 +0300

    feat(frontend): add route loading skeletons

---
+++ frontend/app/orders/loading.tsx
@@ -0,0 +1,5 @@
+import { OrdersPageSkeleton } from "@/components/loading-skeletons";
+
+export default function Loading() {
+  return <OrdersPageSkeleton />;
+}
---
+++ frontend/app/products/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import { ProductDetailSkeleton } from "@/components/loading-skeletons";
+
+export default function Loading() {
+  return <ProductDetailSkeleton />;
+}
